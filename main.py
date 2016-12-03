497e4327938630379ec30b9f7bdbc913d3204ded
Light <18362927193@163.com>
2016-12-04 00:16:08 +0800

    去掉重复的页面

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from numpy import *
 import InputData
 
 
-matrixfile = 'gather.csv'
+matrixfile = 'gather_new.csv'
 OffsetRow = 1
 OffsetCol = 1
 pi = pagerank.pagerankmain(matrixfile, OffsetRow, OffsetCol) #计算网站的pagerank值
@@ -19,6 +19,6 @@ n1 = len(UrlList)
 n2 = pi.shape[1]
 if n1 == n2:
     for i in range(n2):
-        data = {"_id": IdList[i], "url": UrlList[i], "pr": pi[0][i]}
+        data = {"_id": int(IdList[i]), "url": UrlList[i], "pr": pi[0][i]}
         outputer = OutputData.Outputpi()
         outputer.OutputMongo(data)
